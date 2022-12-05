cae7adf7f9b3476f9c1e221e0878a6e7c2655d7e
Jordan Wilia <jordan.k.wilia@gmail.com>
2022-12-05 12:18:18 -0800

    MVP Sidebar design

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Switch } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 export const SideBar = () => {
   return (
-    <Box>SideBar</Box>
+    <Box
+      flex={1}
+      p={2}
+      sx={{display: { xs: "none", sm:"block"}}}
+    >
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <PeopleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <ArticleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <GroupsIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <StorefrontIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <SettingsIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <AccountBoxIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simpe-list">
+              <ListItemIcon>
+                <ModeNightIcon />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+      </List>
+    </Box>
   )
 }
